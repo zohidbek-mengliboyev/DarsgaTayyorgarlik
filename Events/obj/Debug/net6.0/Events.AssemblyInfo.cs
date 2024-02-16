241b96befba39052ac26b9c2d24c0cd503f6811c
zohidbek <uzrivojcoder@gmail.com>
2024-02-17 00:35:24 +0500

    staged changes

--- Events/obj/Debug/net6.0/Events.AssemblyInfo.cs
+++ Events/obj/Debug/net6.0/Events.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Events")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04f0179456b33fa845005aca0f20f27a593460d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e18f6fc6fe0c231f4e50ee1832bf7890fd9746")]
 [assembly: System.Reflection.AssemblyProductAttribute("Events")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Events")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
